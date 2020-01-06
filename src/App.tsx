d5962abd0ce25c03f1ff874f051055792bba3b1b
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-01-06 11:39:04 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import GithubCorner from 'react-github-corner';
 import SortPanel from './components/SortPanel';
 import './css/App.scss';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <GithubCorner
+        href="https://github.com/ethanthatonekid/somesort"
+        octoColor="rgb(20, 22, 26)"
+        bannerColor="rgb(90, 92, 96)"
+      />
       <header className="app-header">Somesort</header>
       <section>
         <SortPanel algorithm="Quicksort" />
